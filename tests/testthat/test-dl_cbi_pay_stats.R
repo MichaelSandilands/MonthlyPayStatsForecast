1f3fc833f664317b497342d188eb2cab6cead963
MichaelSandilands <michael.b.sandilands@gmail.com>
2025-09-01 19:34:52 +0000

    Made tests more robust

--- tests/testthat/test-dl_cbi_pay_stats.R
+++ tests/testthat/test-dl_cbi_pay_stats.R
@@ -15,6 +15,34 @@ test_that("cbi_download downloads and saves a file", {
   # Assert that the file was successfully created at the specified path.
   expect_true(file.exists(dest_path))
 
+  df <- read_excel(dest_path,
+                   sheet = "Master Data")
+
+  # Check number of columns
+  expect_equal(ncol(df), 11)
+
+  # check column names
+  expect_equal(
+    colnames(df),
+    c(
+      'Reporting Period', 'Table', 'Category', 'DSI', 'Channel Type',
+      'Geographical Description', 'Series Description', 'Sector', 'Sub-sector',
+      'Observation Type', 'Observation Value'
+    )
+  )
+
+  # check column types
+  expected_classes <- c(
+    "POSIXct",            # Reporting Period
+    "POSIXt",             # Reporting Period
+    rep("character", 9),  # first 9 are character
+    "numeric"             # Observation Value
+  )
+
+  actual_classes <- lapply(df, class) %>% unlist() %>% unname()
+  expect_equal(actual_classes, expected_classes)
+
   # Clean up the temporary file after the test.
   file.remove(dest_path)
+
 })
